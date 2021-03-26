9136b941673df2d529cc2cdcea04e6fc4b1d4782
talves <tony@3alves.com>
2021-03-26 14:50:53 -0700

    fix dupe

--- src/write.rs
+++ src/write.rs
@@ -1,7 +1,6 @@
 use std::path::PathBuf;
 
 use color_eyre::Result;
-// use std::path::PathBuf;
 
 pub fn write(garden_path: PathBuf, title: Option<String>) -> Result<()> {
     dbg!(garden_path, title);

b261e9b007605e2a23aca480885439e988b30580
talves <tony@3alves.com>
2021-03-26 15:08:01 -0700

    add edit call using

--- src/write.rs
+++ src/write.rs
@@ -1,8 +1,11 @@
+use color_eyre::{eyre::WrapErr, Result};
 use std::path::PathBuf;
-
-use color_eyre::Result;
 
 pub fn write(garden_path: PathBuf, title: Option<String>) -> Result<()> {
     dbg!(garden_path, title);
+    let template = "# ";
+    let content_from_user = edit::edit(template).wrap_err("unable to read writing")?;
+    dbg!(content_from_user);
+
     todo!()
 }

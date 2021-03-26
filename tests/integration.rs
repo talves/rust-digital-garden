25eab592f69038cff641f17910d12f7c66e00403
talves <tony@3alves.com>
2021-03-26 11:42:35 -0700

    return a result also

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,15 +1,18 @@
 use assert_cmd::Command;
+use color_eyre::eyre::Result;
 
 #[test]
-fn test_help() {
+fn test_help() -> Result<()> {
   let mut cmd = Command::cargo_bin("garden").unwrap();
   let assert = cmd.arg("--help").assert();
   assert.success().stderr("");
+  Ok(());
 }
 
 #[test]
-fn test_version() {
+fn test_version() -> Result<()> {
   let mut cmd = Command::cargo_bin("garden").unwrap();
   let assert = cmd.arg("--version").assert();
   assert.success().stderr("");
+  Ok(());
 }

2534c495728a3b9c1885707683d93b06022ac900
talves <tony@3alves.com>
2021-03-26 11:39:28 -0700

    add another test

--- tests/integration.rs
+++ tests/integration.rs
@@ -2,5 +2,14 @@ use assert_cmd::Command;
 
 #[test]
 fn test_help() {
-  unimplemented!();
+  let mut cmd = Command::cargo_bin("garden").unwrap();
+  let assert = cmd.arg("--help").assert();
+  assert.success().stderr("");
+}
+
+#[test]
+fn test_version() {
+  let mut cmd = Command::cargo_bin("garden").unwrap();
+  let assert = cmd.arg("--version").assert();
+  assert.success().stderr("");
 }
